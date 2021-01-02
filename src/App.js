2ca8f2d42482511333bfbc6d07af8bb4a93691bc
Barkley Santo <barkley.santo@gmail.com>
2021-01-02 14:36:53 -0600

    create recipe component, import intop App, export from Recipe component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
+import Recipe from "./Recipe";
 
 
 const App = () => {
   const APP_ID = 'c7e77fe5';
   const APP_KEY = '275dbeadaa5f29f80e32e61261c24bd5';
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
-  const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    console.log('effect has been run')
-  }, []) // request runs once once app gets mounted
+    getRecipes();
+  }, []) // request runs once once app gets mounted - now when counter changes thats when useEffect runs
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
 
   return (
     <div className="App">
@@ -19,7 +25,7 @@ const App = () => {
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+      <Recipe/>
     </div>
 
   )
