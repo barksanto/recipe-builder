d057b5745356039fd8df724188fefab7c69b96f7
Barkley Santo <barkley.santo@gmail.com>
2021-01-02 14:45:36 -0600

    pass props from state to the individual components

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
   }
 
   return (
@@ -25,7 +26,10 @@ const App = () => {
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
-      <Recipe/>
+      {recipes.map(recipe => (
+        <Recipe title={recipe.recipe.label} calories={recipe.recipe.calories}
+        image={recipe.recipe.image}/>
+      ))}
     </div>
 
   )
