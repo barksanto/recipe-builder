da9236d22867b3024f22f272956fabd3c15e7188
Barkley Santo <barkley.santo@gmail.com>
2021-01-02 14:15:37 -0600

    useEffect function only runs once when the page ap is mounted

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ const App = () => {
   const APP_KEY = '275dbeadaa5f29f80e32e61261c24bd5';
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
+  const [counter, setCounter] = useState(0);
+
   useEffect(() => {
     console.log('effect has been run')
-  })
+  }, []) // request runs once once app gets mounted
 
   return (
     <div className="App">
@@ -17,7 +19,9 @@ const App = () => {
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
+
   )
 }
 
