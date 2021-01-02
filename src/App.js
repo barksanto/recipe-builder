ea4e935ca7028c677f438a3c30def3b70f3a6e6b
Barkley Santo <barkley.santo@gmail.com>
2021-01-02 13:43:21 -0600

    get app id and api key for edamam and input into variables interpolated into our request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const app = () => {
+
+  const APP_ID = 'c7e77fe5';
+  const APP_KEY = '275dbeadaa5f29f80e32e61261c24bd5';
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
     </div>
   );
 }
