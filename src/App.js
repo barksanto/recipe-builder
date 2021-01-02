3ac501f19f0feacd1f27c77de694f6a847c230ca
Barkley Santo <barkley.santo@gmail.com>
2021-01-02 14:05:15 -0600

    import useeffect and usestate from react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-const app = () => {
 
+const App = () => {
   const APP_ID = 'c7e77fe5';
   const APP_KEY = '275dbeadaa5f29f80e32e61261c24bd5';
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
+  useEffect(() => {
+    console.log('effect has been run')
+  })
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
